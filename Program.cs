f67a3e33a64ff32af0f39c7f1c85dbb6fea2637a
Nikita Balabaev <nbalabaev@microsoft.com>
2022-07-26 15:23:32 +0200

    Migrate to .NET 6 & update to latest Slack API

--- Program.cs
+++ Program.cs
@@ -1,180 +1,185 @@
-﻿namespace SlackHistoryLoader
+﻿namespace SlackHistoryLoader;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+internal sealed class Program
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Net;
-    using System.Text;
+    private static readonly HttpClient httpClient = new();
+    private static readonly StringBuilder uriBuilder = new();
 
-    using Newtonsoft.Json.Linq;
+    private const int MessagesCount = 100;
+    private const string DownloadDirectory = "conversations";
+    private const string BaseUrl = "https://slack.com/api/";
+    private const string Token = "xoxp-XXXXXXXXXXX-XXXXXXXXXXX-XXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    private const string ListUsersMethod = "users.list";
+    private const string ListChannelsMethod = "conversations.list";
+    private const string HistoryMethod = "conversations.history";
+    private const string ChatDeleteMethod = "chat.delete";
 
-    internal sealed class Program
+    private static async Task Main()
     {
-        private const int MessagesCount = 100;
-        private const string DownloadDirectory = "talks";
-        private const string BaseUrl = "https://slack.com/api/";
-        private const string Token = "token=xoxp-16172012838-16167984613-102407978356-8fe5e0c5b36a6b1f4996a3c2bf44e1d1";
-        private const string ListUsersMethod = "users.list";
-        private const string ListChannelsMethod = "channels.list";
-        private const string ListInstantMessagesChannelsMethod = "im.list";
-        private const string InstantMessagesHistoryMethod = "im.history";
-        private const string ChannelHistoryMethod = "channels.history";
-        private const string ChatDeleteMethod = "chat.delete";
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+        Console.Write("Enter your Slack username: ");
+        var username = Console.ReadLine();
 
-        private static void Main(string[] args)
+        if (!Directory.Exists(DownloadDirectory))
         {
-            Console.Write("Enter your Slack username: ");
-            var username = Console.ReadLine();
-
-            if (!Directory.Exists(DownloadDirectory))
-            {
-                Directory.CreateDirectory(DownloadDirectory);
-            }
-
-            var usersDict = GetApiValues<string, string>(ListUsersMethod, "members", "id", "name");
-
-            if (usersDict.All(u => u.Value != username))
-            {
-                Console.WriteLine("Can't find user '" + username + "'");
-                Console.Write("There are users: " + string.Join(Environment.NewLine, usersDict.Select(u => u.Value)));
-                Console.ReadLine();
-                return;
-            }
-
-            var xakepId = usersDict.First(u => u.Value == username).Key;
-
-            var channelsDict = GetApiValues<string, string>(ListChannelsMethod, "channels", "id", "name");
-            var imsDict = GetApiValues<string, string>(ListInstantMessagesChannelsMethod, "ims", "id", "user");
-
-            Console.WriteLine("Downloadind instant messages...");
-            foreach (var talk in imsDict)
-            {
-                DownloadTalkHistory(false, talk.Key, usersDict[talk.Value], xakepId);
-            }
-
-            Console.WriteLine("Downloadind channel messages...");
-            foreach (var channel in channelsDict)
-            {
-                DownloadTalkHistory(true, channel.Key, channel.Value, xakepId);
-            }
+            Directory.CreateDirectory(DownloadDirectory);
         }
 
-        private static void DownloadTalkHistory(bool isChannel, string talkId, string channelName, string userId, bool deleteMyMessages = false)
+        var usersDict = await GetApiValuesAsync<string, string>(ListUsersMethod, "members", "id", "name");
+
+        var userPair = usersDict.FirstOrDefault(u => u.Value == username);
+
+        if (userPair.Value != username)
         {
-            var oldest = "0";
-            var hasMore = true;
-            while (hasMore)
-            {
-                var history =
-                    MakeApiCall(
-                        CombineApiUrl(
-                            isChannel ? ChannelHistoryMethod : InstantMessagesHistoryMethod,
-                            "channel=" + talkId,
-                            "latest=" + oldest,
-                            "count=" + MessagesCount.ToString()));
-
-                var isOk = history["ok"].Value<bool>();
-                if (!isOk)
-                {
-                    Console.WriteLine(history["error"]);
-                    break;
-                }
-
-                if (history["messages"].Any())
-                {
-                    SaveHistoryToFile(history, channelName + "_" + history["messages"].First["ts"]?.Value<string>());
-                    Console.WriteLine("History from channel '" + channelName + "' saved. Messages: " + history["messages"].Count().ToString());
-                }
-                else
-                {
-                    break;
-                }
-
-                if (deleteMyMessages)
-                {
-                    foreach (var message in history["messages"])
-                    {
-                        if (message["user"].Value<string>() == userId)
-                        {
-                            MakeApiCall(CombineApiUrl(ChatDeleteMethod, "channel=" + talkId, "ts=" + message["ts"].Value<string>()));
-                        }
-                    }
-                }
-
-                oldest = history["messages"].Last["ts"].Value<string>();
-
-                hasMore = history["has_more"].Value<bool>() && !history["is_limited"].Value<bool>();
-            }
+            Console.WriteLine("Can't find user '" + username + "'");
+            Console.Write("There are users: " + string.Join(Environment.NewLine, usersDict.Select(u => u.Value)));
+            Console.ReadLine();
+            return;
         }
 
-        private static void SaveHistoryToFile(JObject history, string fileName)
+        var userId = userPair.Key;
+
+        var channelsDict = await GetApiValuesAsync<string, string>(ListChannelsMethod, "channels", "id", "name", "types=public_channel,private_channel,mpim");
+        var imsDict = await GetApiValuesAsync<string, string>(ListChannelsMethod, "channels", "id", "user", "types=im");
+
+        Console.WriteLine("Downloadind instant messages...");
+        foreach (var conversation in imsDict)
         {
-            using (var file = File.CreateText(Path.Combine(DownloadDirectory, fileName + ".json")))
-            {
-                file.Write(history.ToString());
-            }
+            await DownloadConversationHistoryAsync(false, conversation.Key, usersDict[conversation.Value], userId);
         }
 
-        private static Dictionary<TKey, TValue> GetApiValues<TKey, TValue>(string method, string path, string keyName, string valueName)
+        Console.WriteLine("Downloadind channel messages...");
+        foreach (var channel in channelsDict)
         {
-            var obj = MakeApiCall(CombineApiUrl(method));
-            var isOk = obj["ok"].Value<bool>();
-            var ret = new Dictionary<TKey, TValue>();
-            if (isOk)
+            await DownloadConversationHistoryAsync(true, channel.Key, channel.Value, userId);
+        }
+    }
+
+    private static async Task DownloadConversationHistoryAsync(bool isChannel, string conversationId, string channelName, string userId, bool deleteMyMessages = false)
+    {
+        var oldest = "0";
+        var hasMore = true;
+        while (hasMore)
+        {
+            var history =
+                await MakeApiCallAsync(
+                    CombineApiUrl(
+                        HistoryMethod,
+                        "channel=" + conversationId,
+                        "latest=" + oldest,
+                        "count=" + MessagesCount.ToString()));
+
+            var isOk = history["ok"].Value<bool>();
+            if (!isOk)
             {
-                foreach (var curValue in obj[path])
-                {
-                    var id = curValue[keyName].Value<TKey>();
-                    var name = curValue[valueName].Value<TValue>();
-                    ret.Add(id, name);
-                }
+                Console.WriteLine("Error during loading a conversation history: " + history["error"]);
+                break;
+            }
+
+            JToken messages = history["messages"];
+            if (messages.Any())
+            {
+                await SaveHistoryToFileAsync(history, channelName + "_" + messages.First["ts"]?.Value<string>());
+                Console.WriteLine("History from channel '" + channelName + "' saved. Messages: " + messages.Count().ToString());
             }
             else
             {
-                Console.WriteLine(obj["error"]);
+                break;
             }
 
-            return ret;
+            if (deleteMyMessages)
+            {
+                foreach (var message in messages)
+                {
+                    if (message["user"].Value<string>() == userId)
+                    {
+                        await MakeApiCallAsync(CombineApiUrl(ChatDeleteMethod, "channel=" + conversationId, "ts=" + message["ts"].Value<string>()));
+                    }
+                }
+            }
+
+            oldest = messages.Last["ts"].Value<string>();
+
+            hasMore = history["has_more"].Value<bool>() && !history["is_limited"].Value<bool>();
+        }
+    }
+
+    private static async Task SaveHistoryToFileAsync(JObject history, string fileName)
+    {
+        var path = Path.Combine(DownloadDirectory, fileName + ".json");
+        using var streamWriter = File.CreateText(path);
+        using var jsonWriter = new JsonTextWriter(streamWriter);
+        await history.WriteToAsync(jsonWriter);
+    }
+
+    private static async Task<Dictionary<TKey, TValue>> GetApiValuesAsync<TKey, TValue>(string method, string path, string keyName, string valueName, params string[] parameters)
+    {
+        var obj = await MakeApiCallAsync(CombineApiUrl(method, parameters));
+        var isOk = obj["ok"].Value<bool>();
+        var ret = new Dictionary<TKey, TValue>();
+        if (isOk)
+        {
+            foreach (var curValue in obj[path])
+            {
+                var id = curValue[keyName].Value<TKey>();
+                var name = curValue[valueName].Value<TValue>();
+                ret.Add(id, name);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error during API call: " + obj["error"]);
         }
 
-        private static string CombineApiUrl(string method, params string[] parameters)
-        {
-            var sb = new StringBuilder(BaseUrl);
-            sb.Append(method)
-                .Append('?')
-                .Append(Token);
+        return ret;
+    }
 
+    private static string CombineApiUrl(string method, params string[] parameters)
+    {
+        uriBuilder.Clear();
+        uriBuilder
+            .Append(BaseUrl)
+            .Append(method);
+
+        if (parameters is not null && parameters.Length > 0)
+        {
+            uriBuilder.Append('?');
+
+            bool isFirst = true;
             foreach (var parameter in parameters ?? Array.Empty<string>())
             {
-                sb.Append('&').Append(parameter);
-            }
-
-            return sb.ToString();
-        }
-
-        private static JObject MakeApiCall(string apiUrl)
-        {
-            var request = WebRequest.Create(apiUrl) as HttpWebRequest;
-            var response = request?.GetResponse() as HttpWebResponse;
-
-            if (response == null)
-            {
-                return null;
-            }
-
-            using (var stream = response.GetResponseStream())
-            {
-                if (stream == null)
+                if (isFirst)
                 {
-                    return null;
+                    isFirst = false;
+                }
+                else
+                {
+                    uriBuilder.Append('&');
                 }
 
-                using (var stringStream = new StreamReader(stream))
-                {
-                    return JObject.Parse(stringStream.ReadToEnd());
-                }
+                uriBuilder.Append(parameter);
             }
         }
+
+        return uriBuilder.ToString();
+    }
+
+    private static async Task<JObject> MakeApiCallAsync(string apiUrl)
+    {
+        using var stream = await httpClient.GetStreamAsync(apiUrl);
+        using var textReader = new StreamReader(stream);
+        using var jsonReader = new JsonTextReader(textReader);
+        return await JObject.LoadAsync(jsonReader);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # slack-history
 This tool can download and clear your history in Slack if you want to backup or need to see all messages within free Slack account.
+To generate a token, you'll need to create an app and give it a plenty of permissions. You can follow [this guide](https://github.com/Inumedia/SlackAPI/wiki/Examples#configuring-an-app-on-slack-and-retrieving-an-oauth-token-for-your-application).
